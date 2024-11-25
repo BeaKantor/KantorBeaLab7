cbcc795ef067a9a2782156177e9365f339860b40
Beata Kantor <beata.roberta11@gmail.com>
2024-11-25 15:10:19 +0200

    Laborator8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,27 @@
-﻿namespace KantorBeaLab7
+﻿using System;
+using KantorBeaLab7.Data;
+using System.IO;
+
+
+namespace KantorBeaLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
